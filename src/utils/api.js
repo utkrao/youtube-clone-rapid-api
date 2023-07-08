3cf3d49ed561de442ce7eb6ffead131d2ad79524
utkrao <109218765+ut00@users.noreply.github.com>
2023-07-08 18:01:24 +0530

    youtube clone using rapid API

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,8 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
     params: { hl: "en", gl: "US" },
     headers: {
-        "X-RapidAPI-Key":
-            process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+        "X-RapidAPI-Key": 'd1e483de5fmsh30733711c309ed4p179af4jsn84bcd868f7d9',
         "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
     },
 };
